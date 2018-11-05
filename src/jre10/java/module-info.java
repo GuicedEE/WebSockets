af6025ce30025e0a5b8abb86c44deb573184474f
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:38 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,7 +1,10 @@
 import com.jwebmp.core.base.angular.services.IAngularControllerScopeStatement;
 import com.jwebmp.guicedinjection.interfaces.IGuicePreStartup;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.websockets.injections.LocalStorageKeyWSAuth;
 import com.jwebmp.websockets.injections.WebSocketControllerStatement;
+import com.jwebmp.websockets.injections.WebSocketModuleExclusions;
 import com.jwebmp.websockets.injections.WebSocketsConfiguration;
 import com.jwebmp.websockets.services.IWebSocketAuthDataProvider;
 
@@ -11,7 +14,6 @@ module com.jwebmp.websockets {
 	uses com.jwebmp.websockets.services.IWebSocketSessionProvider;
 	uses com.jwebmp.websockets.services.IWebSocketService;
 
-
 	requires javax.websocket.api;
 	requires java.logging;
 	requires com.jwebmp.logmaster;
@@ -19,6 +21,8 @@ module com.jwebmp.websockets {
 	requires com.jwebmp.core;
 	requires com.fasterxml.jackson.annotation;
 	requires javax.servlet.api;
+	requires java.validation;
+
 	exports com.jwebmp.websockets;
 	exports com.jwebmp.websockets.services;
 
@@ -27,6 +31,9 @@ module com.jwebmp.websockets {
 	provides IGuicePreStartup with WebSocketsConfiguration;
 	provides IAngularControllerScopeStatement with WebSocketControllerStatement;
 	provides IWebSocketAuthDataProvider with LocalStorageKeyWSAuth;
+
+	provides IGuiceScanModuleExclusions with WebSocketModuleExclusions;
+	provides IGuiceScanJarExclusions with WebSocketModuleExclusions;
 
 	opens com.jwebmp.websockets;
 	opens com.jwebmp.websockets.services;
---
+++ src/main/java/com/jwebmp/websockets/injections/WebSocketModuleExclusions.java
@@ -0,0 +1,39 @@
+package com.jwebmp.websockets.injections;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class WebSocketModuleExclusions
+		implements IGuiceScanModuleExclusions<WebSocketModuleExclusions>,
+				           IGuiceScanJarExclusions<WebSocketModuleExclusions>
+{
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-websockets-*");
+		strings.add("commons-io-*");
+		strings.add("commons-lang3-*");
+		strings.add("commons-text-*");
+		strings.add("javax.websocket-api-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.websockets");
+
+		strings.add("org.apache.commons.io");
+		strings.add("org.apache.commons.lang3");
+		strings.add("org.apache.commons.text");
+
+		strings.add("javax.websocket.api");
+		return strings;
+	}
+}
