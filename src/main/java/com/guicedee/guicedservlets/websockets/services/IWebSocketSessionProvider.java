f85a4e2219959c0356215cd16724cf6655b968f6
GedMarc <ged_marc@hotmail.com>
2019-11-06 07:48:34 +0200

    Slight changes to service loading for unit tests

--- src/main/java/com/guicedee/guicedservlets/websockets/services/IWebSocketSessionProvider.java
+++ src/main/java/com/guicedee/guicedservlets/websockets/services/IWebSocketSessionProvider.java
@@ -1,10 +1,12 @@
 package com.guicedee.guicedservlets.websockets.services;
+import com.guicedee.guicedinjection.interfaces.IDefaultService;
+
 import javax.servlet.http.HttpSession;
 
 /**
  * Returns the HTTPSession associated with the given id
  */
-public interface IWebSocketSessionProvider
+public interface IWebSocketSessionProvider extends IDefaultService<IWebSocketSessionProvider>
 {
 	HttpSession getSession(String sessionID);
 }
