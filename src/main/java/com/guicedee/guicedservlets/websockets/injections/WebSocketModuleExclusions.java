7fd5df624f6ea10fe71ece6b66a99e1762ea4d37
GedMarc <ged_marc@hotmial.com>
2020-03-08 17:00:28 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/main/java/com/guicedee/guicedservlets/websockets/injections/WebSocketModuleExclusions.java
+++ src/main/java/com/guicedee/guicedservlets/websockets/injections/WebSocketModuleExclusions.java
@@ -1,6 +1,5 @@
 package com.guicedee.guicedservlets.websockets.injections;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,20 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class WebSocketModuleExclusions
-		implements IGuiceScanModuleExclusions<WebSocketModuleExclusions>,
-				           IGuiceScanJarExclusions<WebSocketModuleExclusions>
+		implements IGuiceScanModuleExclusions<WebSocketModuleExclusions>
 {
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("jwebmp-websockets-*");
-		strings.add("commons-io-*");
-		strings.add("commons-lang3-*");
-		strings.add("commons-text-*");
-		strings.add("javax.websocket-api-*");
-		return strings;
-	}
 
 	@Override
 	public @NotNull Set<String> excludeModules()
